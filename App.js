60d110a1cdd97de6e73114f49f2bdd5cb4387ebb
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-05-24 10:25:37 -0300

    quien verga es gaby

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {SafeAreaView, StyleSheet, TextInput} from "react-native";
 
-export default function App() {
+// Cajas
+const Cajas = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <View
+      style={{
+        flexDirection:"row",
+        height:100,
+        padding:20
+      }}
+    >
+      <View style={{ backgroundColor:"blue", flex:0.3}}/>
+      <View style={{ backgroundColor:"red", flex:0.5}}/>
+      <Text>El pepe</Text>
+      </View>
   );
-}
+};
+
+// Texto de muestra
+const TextoDeMuestra=()=> {
+  const [text,onChangeText]=React.useState("Useless text");
+  const [number,onChangeNumber]=React.useState(null);
+
+  return (
+    <SafeAreaView>
+      <TextInput
+      style={styles.input}
+      onChangeText={onChangeText}
+      value={text}
+      />
+      <TextInput
+      style={styles.input}
+      onChangeText={onChangeNumber}
+      value={number}
+      placeholder="useless placeholder"
+      keyboardType="numeric"
+      />
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  input:{
+    height:40,
+    margon:12,
+    borderWidth:1,
+    padding:10,
+  }
+})
+export default TextoDeMuestra;
